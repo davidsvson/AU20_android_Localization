8379ce6336b187055a4b5f1b01d49cb816e89797
David Svensson <davidsvson@gmail.com>
2020-09-16 10:46:14 +0200

    Added button that changes text

--- app/src/main/java/com/example/localization/MainActivity.kt
+++ app/src/main/java/com/example/localization/MainActivity.kt
@@ -2,16 +2,32 @@ package com.example.localization
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var greetingTextView: TextView
+    var name = "David"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        greetingTextView = findViewById(R.id.textView)
+
     }
+
+
+    fun button_pressed(view: View) {
+       greetingTextView.text = getString(R.string.great, name)
+    }
+
 }
 
 
 // Anpassa appen till svenska och engelska
 // 1. anpassa xml - layout  x
-// 2. anpassa kotlin-fil
+// 2. anpassa kotlin-fil    x
 // 3. anpassa bilder
